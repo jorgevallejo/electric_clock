9500d21e1c3dd19a908ca01cc15398380e67f863
Jorge Vallejo <jorvaor@gmail.com>
2021-09-12 20:58:00 +0200

    Draw second version of clock using polar coordinates
    Also, there are two csv files with the data.
    
    The advantage of this version is that it already has the
    numbers around the circle. The disadvantage is that I
    find more difficult to modify it to my liking.

---
+++ electric_clock2.R
@@ -0,0 +1,16 @@
+# Second take
+# Code from https://stackoverflow.com/questions/18648115/circular-time-plots-in-r-with-stacked-rose
+
+horas <- read.csv("tramos2.csv",
+                  header = TRUE)
+
+library('ggplot2')
+
+p <- ggplot(horas, aes(x = Hora, y = 1, fill = Tarifa)) +
+  geom_bar(breaks = seq(0,24), width = 2, colour = 'grey', stat = 'identity') +
+  theme_minimal() +
+  scale_fill_brewer() +
+  coord_polar(start = 0) +
+  scale_x_continuous("", limits = c(0,24), breaks = seq(0,24), labels = seq(0,24))
+
+p
